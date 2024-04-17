5a3b564eb5ae257af5108efb16020771106ad268
Rishabh Parsediya <parsediyarishabh@gmail.com>
2024-04-17 13:44:49 +0530

    removing the bug for date in calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,43 +12,24 @@ export default function CalendarApp() {
     const [value, onChange] = useState<Value>(new Date());
     const [date, setDate] = useState<string>('');
     const [dateQuery, setDateQuery] = useState('');
-    console.log(value, date, dateQuery)
+    // console.log(value, date, dateQuery)
+
     useEffect(() => {
-        const q = value?.toLocaleString();
-        const query = new Date(String(q));
-        console.log(query);
-        console.log(query.getDate().toString().padStart(2, '0') + "-" + (query.getMonth() + 1).toString().padStart(2, '0') + "-" + query.getFullYear().toString());
-        setDateQuery(String(query.getDate().toString().padStart(2, '0') + "-" + (query.getMonth() + 1).toString().padStart(2, '0') + "-" + query.getFullYear().toString()));
-        const d = value?.toLocaleString().split(',')[0].split('/');
-        if (d !== undefined) {
-            const num: number = Math.floor(parseInt(d[1]) % 10);
-            let sup = '';
-            switch (num) {
-                case 1:
-                    sup = supArray[num - 1];
-                    break;
-                case 2:
-                    sup = supArray[num - 1];
-                    break;
-                case 3:
-                    sup = supArray[num - 1];
-                    break;
-                default:
-                    sup = supArray[3];
-            }
-            const dat = new Date(parseInt(d[2]), parseInt(d[0]) - 1, parseInt(d[1]));
-            const dt: string = (weekdays[dat.getDay()] + ", " + d[1] + sup + " " + months[parseInt(d[0]) - 1] + ", " + d[2]).toString();
-            console.log(dt)
-            setDate(dt);
-        }
-    }, [value])
+        const d = String(value);
+        const dateString = new Date(d);
+        setDate(dateString.toDateString())
+        const year = dateString.getFullYear();
+        const month = (dateString.getMonth() + 1).toString().padStart(2, '0');
+        const date = (dateString.getDate()).toString().padStart(2, '0');
+        setDateQuery(`${date}-${month}-${year}`)
+        console.log(dateQuery);
+    }, [value, dateQuery])
 
     return (
         <div className='flex flex-col justify-center items-center'>
             <Calendar className="w-full" onChange={onChange} value={value} />
             <div className='w-full text-center flex flex-col py-2 gap-y-3'>
-                { }
-                {date && <p className='border-b w-fit mt-2 border-gray-500 text-black text-left'>{date}</p>}
+                <p className='border-b w-fit mt-2 border-gray-500 text-black text-left'>{date}</p>
                 {dateQuery && <Link href={`/expense/${dateQuery}`} className='bg-black uppercase tracking-wider w-full px-4 py-2 text-white rounded-3xl'>
                     Get Expenses at {dateQuery}
                 </Link>}
